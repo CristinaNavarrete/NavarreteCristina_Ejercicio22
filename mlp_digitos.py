cf35215f22e09d5420fe7ba9d3e5b16d7d766463
mc.navarrete2018@uniandes.edu.co <mc.navarrete2018@uniandes.edu.co>
2020-04-17 16:25:30 +0000

    hola2

--- mlp_digitos.py
+++ mlp_digitos.py
@@ -19,37 +19,37 @@ Losses=[]
 F1s_Train=[]
 for i in range(20):
     j=i+1
-    mlp = sklearn.neural_network.MLPClassifier(activation='logistic', hidden_layer_sizes=(j),max_iter=1000)
+    print(j)# Para saber en qué iteración sucede el prob de convergencia
+    mlp = sklearn.neural_network.MLPClassifier(activation='logistic', hidden_layer_sizes=(j),max_iter=2600)
     mlp.fit(X_train, Y_train)
     Losses.append(mlp.loss_)
     F1s_Train.append(sklearn.metrics.f1_score(Y_train, mlp.predict(X_train), average='macro'))
     F1s_Test.append(sklearn.metrics.f1_score(Y_test, mlp.predict(X_test), average='macro'))
-    
+#con 100 it ,con 500 , 1000, 1500,2000 seguía apareciendo probl. de convergencia    
 neuronas=np.arange(1,21)
 plt.figure(figsize=(15,5))
-
 plt.subplot(1,2,1)
 plt.title("Loss")
 plt.plot(neuronas,Losses)
-
 plt.subplot(1,2,2)
 plt.title("F1")
 plt.plot(neuronas,F1s_Train,label='Train')
 plt.plot(neuronas,F1s_Test,label='Test')
 plt.legend()
 plt.savefig('loss_f1.png')
-#Elegimos 6
-
-modelo = sklearn.neural_network.MLPClassifier(activation='logistic', hidden_layer_sizes=(6),max_iter=100)
+#Elegimos 6 porque en test se ve menos cambio después de 6 en 4 todavía ve cambio en F1 en un 10=%
+n_neurons=6
+modelo = sklearn.neural_network.MLPClassifier(activation='logistic', hidden_layer_sizes=(n_neurons),max_iter=2500)
 mlp.fit(X_train, Y_train)
+plt.figure(figsize=(12,12))
 
-plt.figure(figsize=(15,5))
-
-
-for i in range(6):
+for i in range(n_neurons):
     scale = np.max(mlp.coefs_[0])
     plt.subplot(3,2,i+1)
+    plt.title("Neurona "+str(i+1))
     plt.imshow(mlp.coefs_[0][:,i].reshape(8,8),cmap=plt.cm.RdBu, 
                    vmin=-scale, vmax=scale)
-plt.savefig('neuronas.png')      
+plt.savefig('neuronas.png')    
+# No se ven filas o columnas repetidas entonces no son redundantes
+
     
